8d8cd1860f8cfc5c33f0b036aed37bf1e19dc440
Alyssa Davis <Alyssa.Davis@icrossing.com>
2015-06-14 01:07:28 -0700

    headlines

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,10 @@
-window.navViews = [];
-
 // Data for all Navs
 var NavigationCollection = Backbone.Collection.extend({
   url: '/api/nav'
 });
 
-// var NavView = Backbone.View.extend({
-
-// });
-
+// View for each <nav>
 var NavigationView = Backbone.View.extend({
-  // tagName: 'nav',
-  // className: 'nav',
-  // events: {
-  //   'click a': 'open'
-  // },
   template: _.template($('.navigation-view').html()),
 
   initialize: function() {
@@ -24,34 +14,75 @@ var NavigationView = Backbone.View.extend({
   },
 
   render: function() {
-    var self = this;
+    var self = this,
+        model = self.model;
 
-    self.id = self.model.get('title').toLowerCase().replace(/ /, '-');
+    self.id = model.get('title').toLowerCase().replace(/ /, '-');
     self.setElement($('#'+self.id));
 
-    self.$el.html(self.template(self.model.attributes));
+    self.$el.html(self.template(model.attributes));
 
-    return self;
+    // return self;
+  }
+});
+
+// Data for all Headlines
+var HeadlinesCollection = Backbone.Collection.extend({
+  url: '/api/headlines'
+});
+
+// View for each Headline
+var headlineView = Backbone.View.extend({
+  tagName: "li",
+  template: _.template($('.headline-view').html()),
+
+  initialize: function() {
+    var self = this;
+
+    self.listenTo(self.model, 'change', self.render);
+  },
+
+  render: function() {
+    var self = this,
+        model = self.model;
+
+    model.set('teaser', self.truncate(model.get('content')));
+    self.$el.html(self.template(model.attributes));
+
+    $('.headlines').append(self.$el); // need to check for dupes and possibly filter based on keywords
+
+    // return self;
+  },
+
+  // Todo
+  truncate: function(html) {
+    return html;
   }
 });
 
 
-
+// NAVIGATION
 var navs = new NavigationCollection();
 
 navs.on('add', function(nav){
   nav.fetch();
-  window.navViews.push(new NavigationView({
-    model: nav,
-    // id: nav.get('title').toLowercase().replace(/ /, '_')
-  }));
+  new NavigationView({
+    model: nav
+  });
 });
 
-navs.add([{id: 'main.json'}, {id: 'subnav.json'}]);
+navs.add([{id: 'main.json'}, {id: 'subnav.json'}]); // navs.fetch()
 
+// HEADLINES
+var headlines = new HeadlinesCollection();
 
+headlines.on('add', function(headline){
+  headline.fetch();
+  new headlineView({
+    model: headline
+  });
 
+  // $('.headlines').append();
+});
 
-// var titles = books.map(function(book) {
-//   return book.get("title");
-// });
+headlines.add([{id: 'headline1.json'}, {id: 'headline2.json'}]); // headlines.fetch()
